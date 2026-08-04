2f96180da64f2c08691b528587410fd4561dc968
Mike GIllan <mike.gillan@gmail.com>
2026-08-04 14:23:39 +0900

    Add letterboxed editor map view with structural element sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { EditorShell } from './editor/AppShell';
 import { SettingsPanel } from './editor/SettingsPanel';
+import { MapView } from './editor/MapView';
 
 export default function App() {
-  return <EditorShell aside={<SettingsPanel />} />;
+  return <EditorShell aside={<SettingsPanel />} main={<MapView />} />;
 }
